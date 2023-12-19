2aa56e905d466757af87e8de47e8cce8ccb843d9
Korki <44523624+Korkii@users.noreply.github.com>
2023-12-19 21:10:05 +0200

    feature: Finished Directory Class

--- Directory.py
+++ Directory.py
@@ -1,4 +1,14 @@
 from FileClass import File
 
+
 class Dir(File):
-    
+    def __init__(self, parentDir=None):
+        self._fileList = []
+        self._parentDir = parentDir
+
+    def addFileToDir(self, file: File):
+        self._fileList.append(file)
+
+    @property
+    def parentDir(self) -> str:
+        return self._parentDir
